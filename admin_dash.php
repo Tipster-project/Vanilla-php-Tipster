4c1a868e862f84861651215072a96b78996f9a12
Martiiiin756 <martiiiin756@gmail.com>
2015-12-03 11:20:56 +0100

    File upload avalible

--- admin_dash.php
+++ admin_dash.php
@@ -3,10 +3,12 @@
 	session_start();
 ?>
 <div id='team_reg'>
-	<form action='team_reg.php' method='post'>
+	<form action='team_reg.php' method='post' enctype='multipart/form-data'>
 
 		<input type='text' name='country' placeholder='country'></br>
 		<input type='text' name='group_reg' placeholder='group'></br>
+		<input type='file' name='country_flag' accept='image/*' multiple>
+
 		<input type='submit' name='team_reg_btn' value='Registrate'>
 	</form>	
 </div>
--- team_reg.php
+++ team_reg.php
@@ -2,9 +2,17 @@
 <?php
 include 'includes/db_connect.php';
 //Saves the values from the registration form into the database.
-$sql = "INSERT INTO teams (team_name, group_nr)
-VALUES ('" . $_POST['country'] . "', '" . $_POST['group_reg'] . "')";
 
+$sql = "INSERT INTO teams (team_name, group_nr, flag_url)
+VALUES ('" . $_POST['country'] . "', '" . $_POST['group_reg'] . "', '". basename($_FILES['country_flag']['name'])."')";
+
+$target_dir = "img/";
+$target_file = $target_dir . basename($_FILES['country_flag']['name']);
+if (move_uploaded_file($_FILES['country_flag']['tmp_name'], $target_file)) {
+    echo "The file ". basename( $_FILES['country_flag']['name']). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
 if(mysqli_query($db_connect, $sql)){
 	//success
 	echo 'Bean Bag';
