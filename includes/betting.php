2cd62546d68bcc7b0ac1e41bf6fbf11c081781a9
alizhj <lisahjarpe@gmail.com>
2015-12-09 11:45:24 +0100

    Team_groups

--- includes/betting.php
+++ includes/betting.php
@@ -6,6 +6,7 @@ for ($i=0; $i < count($_POST["game_id"]); $i++) {
 	$query = "INSERT INTO bets (game_id, team_id, user_id, tournament_id, goal) VALUES('".$_POST['game_id'][$i]."', '".$_POST['team_id'][$i]."', $user_id, 1, '".$_POST['goal'][$i]."')";
 	if(mysqli_query($db_connect, $query)) {
 		echo "SUccess mf!";
+		// header('Location: ../bet.php');
 	} else {
 		echo "Error: ". $query . "<br>" . mysqli_error($db_connect);
 	}
--- team_groups.php
+++ team_groups.php
@@ -1,16 +1,22 @@
-<?php include 'includes/header.php'; ?>
 
-<!-- ************************************ -->	
-<!-- ************* GROUP A ************** -->
-<!-- ************************************ -->
-<?php
-$queryA = 'SELECT * FROM teams ORDER BY team_points';
+<?
+include 'includes/header.php'; 
 
-$result = mysqli_query($db_connect, $queryA);
+teamGroups('A');
+teamGroups('B');
+teamGroups('C');
+
+
+function teamGroups($teamGroup){
+ include 'includes/db_connect.php';
+
+$query = 'SELECT * FROM teams ORDER BY team_points';
+
+$result = mysqli_query($db_connect, $query);
 $row = mysqli_fetch_assoc($result);
 ?>
 
-<h1>Group A</h1>
+<h1>Group <? echo $teamGroup; ?></h1>
 <table style="border:solid black 1px">
 	<tbody>
 		<tr>
@@ -23,7 +29,7 @@ $row = mysqli_fetch_assoc($result);
 		<?php
 		while($row = $result->fetch_assoc()) {
 			
-			if ( "{$row['group_nr']}"  == "A"){
+			if ( "{$row['group_nr']}"  == $teamGroup){
 				?>
 				<tr>
 					<td><img src="img/<? echo "{$row['team_flag']}"; ?>" /></td>
@@ -37,80 +43,8 @@ $row = mysqli_fetch_assoc($result);
 		?>
 	</tbody>
 </table>
-
-<!-- ************************************ -->	
-<!-- ************* GROUP B ************** -->
-<!-- ************************************ -->
-<?php
-$queryB = 'SELECT * FROM teams ORDER BY team_points';
-
-$resultB = mysqli_query($db_connect, $queryB);
-$rowB = mysqli_fetch_assoc($resultB);
-?>
-
-<h1>Group B</h1>
-<table style="border:solid black 1px">
-	<tbody>
-		<tr>
-			<th></th>
-			<th>Team</th>
-			<th>Points</th>
-			<th>group</th>
-		</tr>
-
-		<?php
-		while($rowB = $resultB->fetch_assoc()) {
-			if ( "{$rowB['group_nr']}"  == "B"){
-				?>
-				<tr>
-					<td><img src="img/<? echo "{$rowB['team_flag']}"; ?>" /></td>
-					<td><? echo "{$rowB['team_name']}"; ?></td>
-					<td><? echo "{$rowB['team_points']}"; ?></td>
-					<td><? echo "{$rowB['group_nr']}"; ?></td>
-				</tr>
-				<?
-			}
-		}	
-		?>
-	</tbody>
-</table>
-
-<!-- ************************************ -->	
-<!-- ************* GROUP C ************** -->
-<!-- ************************************ -->
 <?
-$queryC = 'SELECT * FROM teams ORDER BY team_points ASC';
-
-$resultC = mysqli_query($db_connect, $queryC);
-$rowC = mysqli_fetch_assoc($resultC);
+}
 ?>
-
-<h1>Group C</h1>
-<table style="border:solid black 1px">
-	<tbody>
-		<tr>
-			<th></th>
-			<th>Team</th>
-			<th>Points</th>
-			<th>group</th>
-		</tr>
-
-		<?php
-		while($rowC = $resultC->fetch_assoc()) {
-			
-			if ( "{$rowC['group_nr']}"  == "C"){
-				?>
-				<tr>
-					<td><img src="img/<? echo "{$rowC['team_flag']}"; ?>" /></td>
-					<td><? echo "{$rowC['team_name']}"; ?></td>
-					<td><? echo "{$rowC['team_points']}"; ?></td>
-					<td><? echo "{$rowC['group_nr']}"; ?></td>
-				</tr>
-				<?
-			}
-		}	
-		?>
-	</tbody>
-</table>
 	
 <?php include 'includes/footer.php'; ?>
