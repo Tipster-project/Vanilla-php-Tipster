57657f1f4650d6cfae075a47ac10702252f0f057
alizhj <lisahjarpe@gmail.com>
2015-12-15 10:35:35 +0100

    flaggor tillagt betting

--- includes/betting.php
+++ includes/betting.php
@@ -17,12 +17,21 @@ if(mysqli_query($db_connect, $query)) {
 	 
 	$result2 = $db_connect->query($query2);
 
+
 	?>
 	<form method="post" action="save_bet.php">
 		<?php
+		$games = $result2 ->num_rows /2;
+
+		// for ($i=0; $i < $games ; $i++) { 
+		// 	$row = mysqli_fetch_assoc($result2);
+		// 	echo $row;
+		// }
+
+
 		while($row = mysqli_fetch_assoc($result2)) {
 
-			// var_dump($row);
+			var_dump($row);
 
 			$game_id = $row["game_id"];
 			$team_id = $row["team_id"];
@@ -30,11 +39,13 @@ if(mysqli_query($db_connect, $query)) {
 			$user_id = $_SESSION["user_id"];	
 			$team_name = $row["team_name"];
 			$game_nr = $row["game_number"];
+			$team_flag = $row["team_flag"];
 
 			
 			
+			
 			?>
-			<p><?php echo $team_name . $game_id . $row["team_id"]; ?></p>
+			<p><img src="../img/<?php echo $team_flag ?>" style="width:30px", "height:30px"/><?php echo ' ' . $team_name . $game_id . $row["team_id"]; ?></p>
 			<input type="number" gamenr="<?php echo $game_nr; ?>" name="goal_<?php echo $game_id; ?>"  value="<?php echo $goal; ?>"/></br>
 			<!-- <input type="hidden" name="game_id[]" value="<?php echo $game_id; ?>" /> -->
 			<input type="hidden" name="team_id[]" value="<?php echo $team_id; ?>" />
