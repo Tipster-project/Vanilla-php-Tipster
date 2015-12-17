c4076050474fadceba130168756bbb6503667a06
Martiiiin756 <martiiiin756@gmail.com>
2015-12-17 15:26:47 +0100

    more fixas

--- includes/betting.php
+++ includes/betting.php
@@ -1,6 +1,5 @@
 <?php require "db_connect.php";
 session_start();
-// $tournament_name = $_POST["tournament_name"];
 $tournament_id = $_POST["selected_tournament"];
 $user_id = $_SESSION["user_id"];
 
@@ -8,7 +7,7 @@ $user_id = $_SESSION["user_id"];
 
 $query = "SELECT allGames.*, bets.goal_home, bets.goal_away
 		  FROM
-		  (SELECT T1.team_name as team_home, T2.team_name as team_away, T1.team_flag as home_flag, T2.team_flag as away_flag, T1.team_id as home_team_id, T2.team_id as away_team_id, game_match.*
+		  (SELECT T1.team_name as team_home, T2.team_name as team_away, T1.team_flag as home_flag, T2.team_flag as away_flag, game_match.*
 		  FROM
 		  game_match, teams T1, teams T2
 		  WHERE T1.team_id=game_match.home_team AND
@@ -30,14 +29,12 @@ $query = "SELECT allGames.*, bets.goal_home, bets.goal_away
 		$result = $db_connect->query($query);
 
 	?>
-	<form method="post" action="save_bet.php">
+	<form method="post" action="">
 		<?php
 
 		while($row = mysqli_fetch_assoc($result)) {
 
 			$game_id = $row["game_id"];
-			$home_team_id = $row["home_team_id"];
-			$away_team_id = $row["away_team_id"];
 			$home_name = $row["team_home"];
 			$away_name = $row["team_away"];
 			$home_flag = $row["home_flag"];
@@ -47,16 +44,34 @@ $query = "SELECT allGames.*, bets.goal_home, bets.goal_away
 
 			
 			?>
-			<p><?php echo ' ' . $home_name. ''; ?><img src="../img/<?php echo $home_flag ?>" style="width:30px", "height:30px"/> VS <img src="../img/<?php echo $away_flag ?>" style="width:30px", "height:30px"/><?php echo ' ' . $away_name. ''; ?><input type="number" name="home_goal[]" <?php if(!is_null($goal_home)){ echo 'value="'. $goal_home .'"'; }else{ } ?>/> - <input type="number" name="away_goal[]" <?php if(!is_null($goal_away)){ echo 'value="'. $goal_away .'"'; }else{ } ?>/></p>
-			
-			<input type="hidden" name="home_team_id[]" value="<?php echo $home_team_id; ?>" />
-			<input type="hidden" name="away_team_id[]" value="<?php echo $away_team_id; ?>" />
+			<p><?php echo ' ' . $home_name. ''; ?><img src="../img/<?php echo $home_flag ?>" style="width:30px", "height:30px"/> VS <img src="../img/<?php echo $away_flag ?>" style="width:30px", "height:30px"/><?php echo ' ' . $away_name. ''; ?><input type="number" name="home_goal[]" value="<?php echo $goal_home; ?>" /> - <input type="number" name="away_goal[]" value="<?php echo $goal_away; ?>"/></p>
 			<input type="hidden" name="tournament_id" value="<?php echo $tournament_id; ?>" />
 			<input type="hidden" name="game_id[]" value="<?php echo $game_id; ?>" /></br>
 			<?php
 		}
 		?>
-		<input type="submit" name="save_bets" value="Spara Bets" /> 
 	</form>
-	<?php
+	<button id="check" name="save_bets" value="Spara Bets">spara bets</button>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	var inputs = $('input[type="number"]').each(function() {
+	    $(this).data('original', this.value);
+	});
+
+
+	$('#check').click(function(){
+	    inputs.each(function() {
+	        if ($(this).data('original') !== this.value) {
+	            alert(this + 'has changed');
+	        } else {
+	           //Do nothing
+	        }
+	    });
+	});
+});
+
+</script>
+
+
 
--- includes/save_bet.php
+++ includes/save_bet.php
@@ -6,7 +6,46 @@ $user_id = $_SESSION["user_id"];
 $tournament_id = $_POST['tournament_id'];
 $no_goal_set = null;
 
+for ($i=0; $i < count($_POST['game_id']); $i++) { 
 
+	$query = "SELECT * FROM bets WHERE game_id = '". $_POST['game_id'][$i] ."' AND
+			  user_id = $user_id AND
+			  tournament_id = $tournament_id";
+	$result = $db_connect->query($query);
+	$bet_exist = mysqli_fetch_assoc($result);
+
+	if(is_null($bet_exist)){
+		$query2 = "INSERT INTO bets (game_id, user_id, tournament_id, goal_home, goal_away) VALUES
+				   ('". $_POST['game_id'][$i] ."', $user_id, $tournament_id, '". $_POST['home_goal'][$i] ."', '". $_POST['away_goal'][$i] ."')";
+
+		if(mysqli_query($db_connect, $query2)){
+			//success
+			echo 'Insertion ex:1 succes';
+		}else{
+			// echo a error message if the query dident work.
+			echo "Insert ex:1 Error: ". $query2 . "<br>" . mysqli_error($db_connect);
+		}
+
+	}else{
+		$query3 = "UPDATE bets SET goal_home = '". $_POST['home_goal'][$i] ."' AND goal_away = '". $_POST['away_goal'][$i] ."' WHERE
+		           game_id = '". $_POST['game_id'][$i] ."' AND
+		           user_id = $user_id AND
+		           tournament_id = $tournament_id";
+		echo $query3."<br>";
+
+		if(mysqli_query($db_connect, $query3)){
+			//success
+			//echo 'Update ex:1 succes';
+		}else{
+			// echo a error message if the query dident work.
+			//echo "Update ex:1 Error: ". $query3 . "<br>" . mysqli_error($db_connect);
+		}
+	}
+	//var_dump($bet_exist);
+
+
+
+}
 
 $db_connect->close();
 ?>
