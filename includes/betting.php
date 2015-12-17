c4407f6a5b3d29734d0a5b08b3c1727cd03eac49
Martiiiin756 <martiiiin756@gmail.com>
2015-12-17 12:00:20 +0100

    betting done (i think) :P

--- includes/betting.php
+++ includes/betting.php
@@ -6,45 +6,53 @@ $user_id = $_SESSION["user_id"];
 
 
 
-	$query2 = "SELECT teams.*, games.*, bets.goal
-				FROM games 
-				LEFT JOIN teams on games.team_name = teams.team_name
-				LEFT JOIN bets on games.game_id = bets.game_id";
+$query = "SELECT allGames.*, bets.goal_home, bets.goal_away
+		  FROM
+		  (SELECT T1.team_name as team_home, T2.team_name as team_away, T1.team_flag as home_flag, T2.team_flag as away_flag, T1.team_id as home_team_id, T2.team_id as away_team_id, game_match.*
+		  FROM
+		  game_match, teams T1, teams T2
+		  WHERE T1.team_id=game_match.home_team AND
+		  T2.team_id=game_match.away_team) as allGames
+		  
+		  LEFT OUTER JOIN 
+		  (
+		  SELECT
+		  *
+		  FROM
+		  bets
+		  WHERE
+		  user_id = $user_id AND
+		  tournament_id = $tournament_id
+		  ) as bets
+		  ON
+		  allGames.game_id = bets.game_id";
 
-	 
-	$result2 = $db_connect->query($query2);
-
+$result = $db_connect->query($query);
 
 	?>
 	<form method="post" action="save_bet.php">
 		<?php
-		$games = $result2 ->num_rows /2;
 
-		// for ($i=0; $i < $games ; $i++) { 
-		// 	$row = mysqli_fetch_assoc($result2);
-		// 	echo $row;
-		// }
-
-
-		while($row = mysqli_fetch_assoc($result2)) {
-
-			var_dump($row);
+		while($row = mysqli_fetch_assoc($result)) {
 
 			$game_id = $row["game_id"];
-			$team_id = $row["team_id"];
-			$goal = $row["goal"];
-			$user_id = $_SESSION["user_id"];	
-			$team_name = $row["team_name"];
-			$game_nr = $row["game_number"];
-			$team_flag = $row["team_flag"];
+			$home_team_id = $row["home_team_id"];
+			$away_team_id = $row["away_team_id"];
+			$home_name = $row["team_home"];
+			$away_name = $row["team_away"];
+			$home_flag = $row["home_flag"];
+			$away_flag = $row["away_flag"];
+			$goal_home = $row["goal_home"];
+			$goal_away = $row["goal_away"];
 
 			
 			?>
-			<p><img src="../img/<?php echo $team_flag ?>" style="width:30px", "height:30px"/><?php echo ' ' . $team_name. ''; ?></p>
-			<input type="number" gamenr="<?php echo $game_nr; ?>" name="goal_<?php echo $game_id; ?>"  value="<?php if(!is_null($goal)){ echo $goal; }else{ } ?>"/></br>
-			<input type="hidden" name="game_id[]" value="<?php echo $game_id; ?>" />
-			<input type="hidden" name="team_id[]" value="<?php echo $team_id; ?>" />
+			<p><?php echo ' ' . $home_name. ''; ?><img src="../img/<?php echo $home_flag ?>" style="width:30px", "height:30px"/> VS <img src="../img/<?php echo $away_flag ?>" style="width:30px", "height:30px"/><?php echo ' ' . $away_name. ''; ?><input type="number" name="home_goal[]" <?php if(!is_null($goal_home)){ echo 'value="'. $goal_home .'"'; }else{ } ?>/> - <input type="number" name="away_goal[]" <?php if(!is_null($goal_away)){ echo 'value="'. $goal_away .'"'; }else{ } ?>/></p>
+			
+			<input type="hidden" name="home_team_id[]" value="<?php echo $home_team_id; ?>" />
+			<input type="hidden" name="away_team_id[]" value="<?php echo $away_team_id; ?>" />
 			<input type="hidden" name="tournament_id" value="<?php echo $tournament_id; ?>" />
+			<input type="hidden" name="game_id[]" value="<?php echo $game_id; ?>" /></br>
 			<?php
 		}
 		?>
--- includes/save_bet.php
+++ includes/save_bet.php
@@ -4,35 +4,9 @@ session_start();
 //Finlir med isset knapp osv behöver kirras.
 $user_id = $_SESSION["user_id"];
 $tournament_id = $_POST['tournament_id'];
+$no_goal_set = null;
 
-$query = "SELECT * FROM bets WHERE tournament_id = $tournament_id AND user_id = $user_id";
-$result = $db_connect->query($query);
 
-$row = mysqli_fetch_assoc($result);
-$old_goal = $row['goal'];
-for ($i=0; $i < count($_POST["game_id"]); $i++) { 
-
-		if( $old_goal != '') {
-			$query2 = "UPDATE bets SET game_id = '".$_POST['game_id'][$i]."', team_id = '".$_POST['team_id'][$i]."', user_id = $user_id, tournament_id = $tournament_id, goal = '".$_POST['goal'][$i]."'";
-			if(mysqli_query($db_connect, $query2)){
-				echo "Success mf!";
-			} else {
-				echo "Error: ". $query2 . "<br>" . mysqli_error($db_connect);
-			}
-		}
-		else {
-			$query3 = "INSERT INTO bets (game_id, team_id, user_id, tournament_id, goal) 
-					VALUES('".$_POST['game_id'][$i]."', '".$_POST['team_id'][$i]."', $user_id, $tournament_id, '".$_POST['goal'][$i]."')";
-	
-			if(mysqli_query($db_connect, $query3)) {
-				echo "Success mf!";
-			}
-	
-		else {
-			echo "Error: ". $query3 . "<br>" . mysqli_error($db_connect);
-		}
-	
-}
 
 $db_connect->close();
 ?>
