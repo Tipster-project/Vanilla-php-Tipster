f5bb33acabe3d456538443ea59f48ce0dc9e7c8a
Martiiiin756 <martiiiin756@gmail.com>
2016-01-07 16:53:24 +0100

    some stuff

--- create_group.php
+++ create_group.php
@@ -2,19 +2,24 @@
 <?php include 'includes/menu.php'; ?>
 <div class="container">
 	<div class="row">
-		<div id='tournament_reg col-sm-12'>
-			<h2>Skapa grupp</h2>
-			<form id="creat_group_form" action='reg_tournament.php' method='post'>
+		<div id='tournament_reg' class='col-sm-12'>
+			<h2>Skapa grupp</h2><br/>
+			<form id="creat_group_form" class="form-group" action='reg_tournament.php' method='post'>
 				<div class="picture">
-					<input id="groupname" type='text' name='tournament_name' placeholder='Group name'>
+					<label>Grupp namn:</label>
+					<input id="groupname" class="form-control" type='text' name='tournament_name' placeholder='Lisas tävling'>
 					<img id="checked" style="display: none" class="hideImg" width="25px" height="25px" src="img/checked.png"><img id="uunchecked" style="display: none" class="hideImg" width="25px" height="25px" src="img/unchecked.png">
 					<!--The p tag will contain the error messages for what is wrong with the value in input-->
 					<p id="message">&nbsp;</p>
 
 				</div>
-				<textarea name='tournament_text' correct="correct" placeholder='Put groupinformation here!'></textarea></br>
-				<input name="invitation-email" correct="correct" placeholder='Invite people!'></br>
-				<button type='submit' name='tournament_reg_btn' value='skapa grupp'>Skapa Grupp</button>
+				<label>Skriv en grupp text:</label>
+				<textarea class="form-control" rows="5" name='tournament_text' correct="correct" placeholder="Här ska vi bara ha de skoj! inget funsk nu va!!"></textarea></br>
+				<br/>
+				<label>Skriv i mail adress till de som du vill bjuda in till gruppen med ett mellan slag mellan adresserna</label>
+				<input class="form-control" name="invitation-email" correct="correct" placeholder='exempel@exempel.se exempel@exempel.se'></br>
+				<br/>
+				<button id="create_btn" class="btn btn-default" type='submit' name='tournament_reg_btn' value='skapa grupp'>Skapa Grupp</button>
 				<p id="big-message">&nbsp;</p>
 			</form>
 		</div>
--- reg_tournament.php
+++ reg_tournament.php
@@ -7,6 +7,7 @@ $T_text = $_POST['tournament_text'];
 $user_id = $_SESSION['user_id'];
 $user_name = $_SESSION['user_name'];
 //$email_string = $_POST['invitation-email'];
+//$emails = explode(' ', $email_string);
 
 
 $sql = "INSERT INTO tournament (tournament_name, user_id, tournament_text) 
@@ -14,7 +15,6 @@ VALUES ('". $T_name ."', '". $user_id ."', '". $T_text ."') ";
 
 if(mysqli_query($db_connect, $sql)){
 	//success
-	echo 'Bean Bag';
 }else{
 	// echo a error message if the query dident work.
 	echo "Error: ". $sql . "<br>" . mysqli_error($db_connect);
@@ -22,14 +22,20 @@ if(mysqli_query($db_connect, $sql)){
 
 
 $tournament_id = mysqli_insert_id($db_connect);
-$sql = "INSERT INTO user_tournaments (user_id, user_name, tournament_id) VALUES ('". $user_id ."', '". $user_name ."','". $tournament_id ."')";
+$sql = "INSERT INTO user_tournaments (user_id, user_name, tournament_id) 
+VALUES ('". $user_id ."', '". $user_name ."','". $tournament_id ."')";
 
 if(mysqli_query($db_connect, $sql)){
 	//success
-	echo 'Bean Bag';
-	header("Location: tournaments.php");
 }else{
 	// echo a error message if the query dident work.
 	echo "Error: ". $sql . "<br>" . mysqli_error($db_connect);
 }
+
+//$subject = 'Din vän '. $user_name . 'har bjudit in dig till Tippzter';
+//$txt = 'För att gå med i '. $user_name .' grupp och registrera dig, tryck på länken';
+//$header = ''
+//foreach ($emails as $email) {
+	
+//}
  ?>
